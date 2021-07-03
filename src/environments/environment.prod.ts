f1e0afd9fe491a538df8a42e1d1ad57088ef6a6a
Alexander Marcos <marcos.valdez@hotmail.com>
2021-07-03 05:06:32 -0500

    heroku 3.0

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
   api: {
-    baseUrl: 'https://pruebademo2s.herokuapp.com/api/v1/',
-    //baseUrl: 'http://localhost:8080/api/v1/',
+    baseUrl: 'https://pruebademo2s.herokuapp.com/api/v1/'
   }
 };
